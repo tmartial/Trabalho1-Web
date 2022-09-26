06fbc6fe690fe90cb05e4b08a9fd0c7abdb1ed2f
THOMAS MARTIAL <u2220075@TAPAJOS.LAB-GRAD>
2022-09-26 14:52:03 -0300

    Coloring alphabet buttons on click and generating random word

--- Javascript/game.js
+++ Javascript/game.js
@@ -4,11 +4,26 @@ onload = function () {
     var alphabet = generateAlphabet();
     console.log(alphabet);
     listButton = generateButtonAlphabet(alphabet)
+    var words = ['CHAIR', 'TABLE', 'CAR'];
+    wordToFind = chooseWordsAleatory(words);
+    listLetter = generateWordToFind(wordToFind);
 }
 
 //Image list
 var imagesPath = ['IMG\1-HangHim.jpg','IMG\2-HangHim.jpg', 'IMG\3-HangHim.jpg', 'IMG\4-HangHim.jpg', 'IMG\5-HangHim.jpg',
                     'IMG\6-HangHim.jpg', 'IMG\7-HangHim.jpg', 'IMG\8-HangHim.jpg',]
+
+
+
+function chooseWordsAleatory(listWord) {
+    var lengthListWord = listWord.length;
+    console.log(lengthListWord)
+    var random = Math.floor(Math.random()*lengthListWord)
+    console.log('The random number is :', random)
+    var wordToFind = listWord[random];
+    console.log(wordToFind);
+    return wordToFind;
+}
 
 
 function generateAlphabet() {
@@ -27,8 +42,25 @@ function generateButtonAlphabet(alphabet){
         button.type = 'button';
         button.innerHTML = alphabet[i];
         button.id = "idButton"+i;
-        listButton.push(button)
+        button.addEventListener("click", function() {
+            this.style.backgroundColor = "grey";
+            this.style.color = 'white';
+          });
+        listButton.push(button);
         containerButton.appendChild(listButton[i]);     
     }
     return listButton;
+ }
+
+ function generateWordToFind(word){
+    var listLetter = []
+    var containerWord = this.document.getElementById("idWordToFind");
+    for (var i = 0; i < word.length; i++ ){
+        var letter = document.createTextNode(word[i]);
+        letter.textContent = `_ `;
+        listLetter.push(word[i]);
+        console.log(listLetter);
+        containerWord.appendChild(letter);  
+    }
+    return listLetter;
  }
