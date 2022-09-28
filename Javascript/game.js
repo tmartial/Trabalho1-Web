2516e88db1ffe614ca2fb91486f257f5775d168d
Thomas Martial <thomas.martial@insa-lyon.fr>
2022-09-28 00:00:30 -0300

    Modification in gamePage.html and game.js, button connected to letter

--- Javascript/game.js
+++ Javascript/game.js
@@ -3,10 +3,12 @@ onload = function () {
     var figura = document.getElementById("idImageHangHim");
     var alphabet = generateAlphabet();
     console.log(alphabet);
-    listButton = generateButtonAlphabet(alphabet)
-    var words = ['CHAIR', 'TABLE', 'CAR'];
-    wordToFind = chooseWordsAleatory(words);
-    listLetter = generateWordToFind(wordToFind);
+    var wordsMedium = ['CHAIR', 'TABLE', 'STARS'];
+    var wordToFind = chooseWordsAleatory(wordsMedium);
+    var wordInfos = generateWordToFind(wordToFind);
+    var listLetter = wordInfos[0];
+    var listNodeLetter = wordInfos[1];
+    var listButton = generateButtonAlphabet(alphabet, listLetter, listNodeLetter);
 }
 
 //Image list
@@ -34,7 +36,7 @@ function generateAlphabet() {
     return alphabet;
 }
 
-function generateButtonAlphabet(alphabet){
+function generateButtonAlphabet(alphabet, listLetter, listNodeLetter ){
     var listButton = []
     var containerButton = this.document.getElementById("idAlphabetButton");
     for (var i = 0; i < alphabet.length; i++ ){
@@ -45,22 +47,47 @@ function generateButtonAlphabet(alphabet){
         button.addEventListener("click", function() {
             this.style.backgroundColor = "grey";
             this.style.color = 'white';
+            letter = getTextButton(this.id);
+            compareLetterToWord(letter, listLetter, listNodeLetter)
           });
         listButton.push(button);
         containerButton.appendChild(listButton[i]);     
     }
-    return listButton;
+    return listButton
  }
 
  function generateWordToFind(word){
     var listLetter = []
+    var listNodeLetter = []
     var containerWord = this.document.getElementById("idWordToFind");
     for (var i = 0; i < word.length; i++ ){
-        var letter = document.createTextNode(word[i]);
-        letter.textContent = `_ `;
+        var nodeLetter = document.createTextNode(word[i]);
+        nodeLetter.id = nodeLetter[i]
+        nodeLetter.textContent = `_ `;
         listLetter.push(word[i]);
+        listNodeLetter.push(nodeLetter);
         console.log(listLetter);
-        containerWord.appendChild(letter);  
+        containerWord.appendChild(nodeLetter);  
     }
-    return listLetter;
+    return [listLetter, listNodeLetter];
  }
+
+ function getTextButton(idButton){
+    console.log(idButton)
+    var containerButtonCliqued = this.document.getElementById("idButtonCliqued");
+    letter = document.getElementById(idButton).textContent;
+    nodeLetter = document.createTextNode(letter); 
+    containerButtonCliqued.appendChild(nodeLetter)
+    return letter
+ }
+
+function compareLetterToWord(letter, listLetter, listNodeLetter){
+    for (var i = 0; i < listNodeLetter.length; i++){
+        console.log(letter)
+        console.log(listNodeLetter[i].textContent)
+        console.log(listLetter[i])
+        if (letter == listLetter[i]){
+            listNodeLetter[i].textContent = letter
+        }
+   }
+}
